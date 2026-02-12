2b5dc2b94f5dfa275ba19a69230a37bd70395609
KAROLL STEFANNY MOORE LOPEZ <karollmoore1141863@correo.itm.edu.co>
2026-02-12 17:37:52 -0500

    The greatest number

---
+++ LogiConcepts/LogicConcepts/TheGreathestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    Console.WriteLine("Ingrese 3 numero diferentes");
+    var a = ConsoleExtension.GetInt("ingrese primer numero  :");
+    var b = ConsoleExtension.GetInt("ingrese segundo numero :");
+    var c = ConsoleExtension.GetInt("ingrese tercer numero  :");
+
+    if (a > b && a >c)
+    {
+        Console.WriteLine($"El numero mayor es: {a}");
+    }
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El numero mayor es: {b}");
+    }
+    else
+    {
+        Console.WriteLine($"El numero mayor es: {c}");
+    }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game Over.");
