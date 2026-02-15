89b9e8b1c7828787f380c685060bbb97678b6d58
KAROLL STEFANNY MOORE LOPEZ <karollmoore1141863@correo.itm.edu.co>
2026-02-15 15:20:50 -0500

    is multiple of another

---
+++ LogiConcepts/LogicConcepts/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,16 @@
+﻿using Shared;
+
+
+do
+{
+    var a = ConsoleExtension.GetInt("ingrese primer numero:  ");
+    var b = ConsoleExtension.GetInt("ingrese segundo numero: ");
+    if (b % a == 0)
+    {
+        Console.WriteLine($"{a} es multiplo {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es multiplo {b}");
+    }
+} while (true);
