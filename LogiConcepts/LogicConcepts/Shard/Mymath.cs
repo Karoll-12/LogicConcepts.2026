0e746bb14a82515451b825860b852dd0fb8943f9
KAROLL STEFANNY MOORE LOPEZ <karollmoore1141863@correo.itm.edu.co>
2026-02-18 22:14:03 -0500

    Factorial

---
+++ LogiConcepts/LogicConcepts/Shard/Mymath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+   public class Mymath
+    {
+        public static double Factorial(int n)
+        {
+            double factorial = 1;
+            for (int i = 2; i <=  n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
